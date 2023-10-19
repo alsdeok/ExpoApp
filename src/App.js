bc6beb3fef58270335aa0766bb9b16a4a01e8fe2
alsdeok <axel.s09kpo@gmail.com>
2023-10-19 07:57:31 -0300

    Falta descripcion

--- src/App.js
+++ src/App.js
@@ -7,80 +7,234 @@ import videoFormulario from "./videos/VideoFormulario.mp4";
 import videoListado from "./videos/VideoListado.mp4";
 import videoPerfil from "./videos/VídeoPerfil.mp4";
 import videoMultimedia from "./videos/VideoMultimedia.mp4";
+import { Carousel, Typography } from "@material-tailwind/react";
+import { useState } from "react";
+
 function App() {
+  const [estadoDiv, setEstadoDiv] = useState(true);
   const videos = [
     {
-      descripcion: "Apartado de ",
-      titulo: "Landing Page",
+      descripcion: [
+        "Una primera vista sencilla, diseñada por la misma empresa.",
+        "Tiene un background cambiante en base al display en uso.",
+        "Cuenta con una navbar sencilla la cual permite el ingreso a la aplicacion y un footer con datos relevantes, ambos presentes en todo momento.",
+      ],
+      titulo: "Pagina de Inicio",
       video: video,
     },
     {
-      descripcion: "Apartado de Perfil ",
-      titulo: "Landing Page",
+      descripcion: [
+        "Muestra una carta simple con informacion personal y la opcion de",
+        "solicitar solicitar nivel de acceso.",
+        "Todo de manera responsive.",
+      ],
+      titulo: "Perfil Personal",
       video: videoPerfil,
     },
     {
-      descripcion: "Apartado de Contacto ",
-      titulo: "Apartado Contacto",
+      descripcion: [
+        "Un formulario con acceso mediante el footer.",
+        "Dentro de la app se visualiza un listado de los mensajes con su estado, con la posibilidad de eliminar, ampliar o responder.",
+        "Si es respondido, se envia un email con la respuesta.",
+      ],
+      titulo: "Apartado de Contacto",
       video: videoContacto,
     },
     {
-      descripcion: "Apartado de Multimedia ",
-      titulo: "Apartado Multimedia",
+      descripcion: [
+        "Apartado destinado a cargar y descargar archivos en formatos para PDF, Video, Power Point, y otros necesarios.",
+        "Al contar la empresa con dos sedes, necesariamente los archivos son discriminados por el lugar establecido.",
+        "Todo su contenido es responsive.",
+      ],
+      titulo: "Sector de Multimedia",
       video: videoMultimedia,
     },
     {
-      descripcion: "Apartado de Eventos ",
+      descripcion: ["Apartado de Eventos "],
       titulo: "Apartado Eventos",
       video: videoEventos,
     },
     {
-      descripcion: "Apartado de Reuniones ",
+      descripcion: ["Apartado de Reuniones "],
       titulo: "Apartado Reuniones",
       video: VideoReuniones,
     },
     {
-      descripcion: "Apartado de Formulario ",
+      descripcion: ["Apartado de Formulario "],
       titulo: "Apartado Formulario",
       video: videoFormulario,
     },
     {
-      descripcion: "Apartado de Listado ",
+      descripcion: ["Apartado de Listado "],
       titulo: "Apartado Listado",
       video: videoListado,
     },
   ];
 
   return (
-    <div className="App">
-      <div className="App-header">
-        <h1>Mi Aplicacion Web</h1>
-        <h3>
+    <div>
+      <div className="text-center min-h-screen flex flex-col items-center justify-center text-white text-2xl p-5 border-b-2 border-dotted border-black bg-[#282c34]">
+        <h1 className="mb-10 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">
+          Mi Aplicacion Web
+        </h1>
+        <h3 className="mb-6 text-lg font-normal text-gray-400 lg:text-xl sm:px-16 xl:px-48">
           Esta app es usada actualmente para una empresa que necesita mantener
           una organizacion de su personal, elementos, eventos y demas, de manera
-          simple, sencilla y de facil interaccion.
+          simple, sencilla y de facil interaccion. Cuando gustes desliza para
+          explorar las distintas partes que la componen.
         </h3>
-        <h4>
-          Puedes ir bajando para explorar las distintas partes que la componen
-        </h4>
-        <p>
+        <p className="mb-6 text-lg font-normal text-gray-400 lg:text-xl sm:px-16 xl:px-48">
           Su exploracion consiste en una serie de videos por cada parte de la
-          pagina, podras comprender que, por motivos de privacidad, cualquier
+          pagina. Podras comprender que, por motivos de privacidad, cualquier
           nombre y/o palabra relacionada a la empresa o a mi persona que
           aparezca en el video, tendra que ser censurado.
         </p>
-        <div className="arrow"></div>
-      </div>
 
-      {videos.map((video) => {
-        return (
-          <div key={video.descripcion} className="App-header">
-            <h1>{video.titulo}</h1>
-            <p>{video.descripcion}</p>
-            <video src={video.video} width="800" height="500" controls />
+        <svg
+          className="mt-10 w-20 h-20 text-white-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 8"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
+          />
+        </svg>
+      </div>
+      <div className="text-center min-h-screen flex flex-col items-center justify-center text-white text-2xl p-10 border-b-2 border-dotted border-black bg-[#282c34]">
+        <h1 className="mb-10 text-4xl font-extrabold leading-none tracking-tight text-white md:text-5xl lg:text-6xl">
+          Caracteristicas Generales
+        </h1>
+        <div className="mb-6 text-lg font-normal text-gray-400 lg:text-xl sm:px-16 xl:px-48">
+          La aplicacion cuenta con una arquitectura que contiene:
+          <ul>
+            <li className="text-justify">
+              <p className="text-decoration-line: underline inline">
+                FrontEnd:
+              </p>{" "}
+              Una aplicacion construida con ReactJS, y con el uso de
+              React-Router, Redux y tailwindcss.
+            </li>
+            <li className="text-justify">
+              <p className="text-decoration-line: underline inline">BakcEnd:</p>{" "}
+              Una aplicacion construida en un entorno de NodeJS, mediante el uso
+              del framework Express, el cual define controladores y servicios
+              que interactuan con una base de datos PostgresSQL mediante el ORM
+              Sequelize. El servidor se encuentra alojado en distintas
+              instancias en amazon medieante el servicio de EC2.
+            </li>
+          </ul>
+          <hr className="h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+          <div className="flex flex-col md:flex-row justify-around mt-4 border-r border-l border-white">
+            <div className="md:border-r p-2 md:pr-10">
+              <p className="text-decoration-line: underline inline">
+                {" "}
+                Otras tecnologias y frameworks utilizados son:{" "}
+              </p>
+              <ul className="mt-2">
+                <li>Express-session</li>
+                <li>Jsonwebtoken</li>
+                <li>Morgan</li>
+                <li>Multer</li>
+                <li>Nodemailer</li>
+                <li>Passport</li>
+                <li>Google-oauth2</li>
+              </ul>
+            </div>
+            <hr className="md:hidden h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
+            <div>
+              <p className="text-decoration-line: underline inline">
+                {" "}
+                Principales utilidades:
+              </p>
+              <ul className="mt-2">
+                <li>Organizacion</li>
+                <li>Listados</li>
+                <li>Calendario de eventos</li>
+                <li>Uso de sesiones</li>
+                <li>Administracion de permisos</li>
+                <li>Autorizacion mediante Google</li>
+              </ul>
+            </div>
           </div>
-        );
-      })}
+        </div>
+        <svg
+          className="mt-10 w-20 h-20 text-white-800 dark:text-white"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 14 8"
+        >
+          <path
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="m1 1 5.326 5.7a.909.909 0 0 0 1.348 0L13 1"
+          />
+        </svg>
+      </div>
+      <Carousel className="bg-[#282c34]">
+        {videos.map((video) => {
+          return (
+            <div key={video.id}>
+              <div className="relative h-screen w-full flex items-center place-content-center">
+                <div className="flex justify-center w-full">
+                  <video
+                    src={video.video}
+                    className="rounded-xl"
+                    width="800"
+                    height="500"
+                    controls
+                  />
+                </div>
+                {estadoDiv ? null : (
+                  <button
+                    className="absolute text-lg font-semibold top-20  md:right-10 md:top-10 bg-white/30 hover:bg-white rounded-lg px-2 py-1"
+                    onClick={() => setEstadoDiv(true)}
+                  >
+                    Detalle
+                  </button>
+                )}
+                {estadoDiv ? (
+                  <div className="absolute inset-0 grid h-full w-full place-items-center bg-black/90">
+                    <button
+                      className="absolute text-lg font-semibold top-20 md:right-10 md:top-10 bg-white/50 hover:bg-white rounded-lg px-3 py-1"
+                      onClick={() => setEstadoDiv(false)}
+                    >
+                      Cerrar
+                    </button>
+
+                    <div className="w-3/4 text-center md:w-2/4">
+                      <Typography
+                        variant="h1"
+                        color="white"
+                        className="mb-4 text-3xl md:text-4xl lg:text-5xl"
+                      >
+                        {video.titulo}
+                      </Typography>
+                      <Typography
+                        variant="lead"
+                        color="white"
+                        className="mb-12 opacity-80"
+                      >
+                        {video.descripcion.map((parrafo, index) => (
+                          <p key={index}>{parrafo}</p>
+                        ))}
+                      </Typography>
+                    </div>
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          );
+        })}
+      </Carousel>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+
+const withMT = require("@material-tailwind/react/utils/withMT");
+module.exports = withMT({
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+});
